a5b33836c421c535ee157af369e198aeca72e0de
t3rr0r <mail@t3rr0r.com>
2014-09-29 10:23:31 -0400

    handle embedded youtube playlists

--- tests/youtube.js
+++ tests/youtube.js
@@ -42,3 +42,43 @@ describe('Analyzing http://www.youtube.com/watch?v=iODdvJGpfIA', function () {
 	items[0].created_at.should.equalDate.date;
     });
 });
+
+describe('Analyzing http://www.youtube.com/embed/videoseries?list=PLpl1Jk0Vplb-lrkbPDcI89qlolGqrc8ah', function () {
+    var items;
+
+    before(function(done) {
+	parse('http://www.youtube.com/embed/videoseries?list=PLpl1Jk0Vplb-lrkbPDcI89qlolGqrc8ah', function(err, i) {
+	    items = i;
+	    done();
+	});
+    });
+
+    it('identify as a youtube video', function() {
+	items.should.have.length(15);
+    });
+
+    it('first video should be UpRssA0CQ0E', function() {
+	items[0].id.should.equal('UpRssA0CQ0E');
+    });
+
+    it('host should be youtube', function() {
+	items[0].host.should.equal('youtube');
+    });
+
+    it('url should be http://www.youtube.com/watch?v=UpRssA0CQ0E', function() {
+	items[0].url.should.equal('http://www.youtube.com/watch?v=UpRssA0CQ0E');
+    });
+
+    it('title should be LOUDPVCK & GLADIATOR feat. Nipsey Hussle - Tony (Official Video)', function() {
+	items[0].title.should.equal('LOUDPVCK & GLADIATOR feat. Nipsey Hussle - Tony (Official Video)');
+    });
+
+    it('duration should be 156', function() {
+	items[0].duration.should.equal('156');
+    });
+
+    it('created_at should be 2014-09-11T15:07:35.000Z', function() {
+	var date = new Date('2014-09-11T15:07:35.000Z');
+	items[0].created_at.should.equalDate.date;
+    });
+});
