05e6de61dfb6b3c0f5ca327315d2f2c073d05bfb
t3rr0r <mail@t3rr0r.com>
2017-02-10 13:03:19 -0500

    clean up

--- index.js
+++ index.js
@@ -1,2 +1,52 @@
 /* global module, require */
-module.exports = require('./lib/parse');
+var async = require('async'),
+    fs = require('fs'),
+    path = require('path'),
+    URI = require('urijs');
+
+function isURL (text) {
+  var pattern = '^(https?:\\/\\/)?' + // protocol
+	'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+	'((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+	'(?::\\d{2,5})?' + // port
+	'(?:/[^\\s]*)?$'; // path
+
+  var re = new RegExp(pattern, 'i');
+  return re.test(text);
+}
+
+module.exports = function(url, callback) {
+  var extractors, extractorsDir, extractorsFiles, result, iterator, uri;
+
+  if (!isURL(url))
+    return callback('Not a valid url')
+
+  extractorsDir = __dirname + '/lib/extractors';
+  extractorsFiles = fs.readdirSync(extractorsDir).sort();
+  extractors = [];
+
+  for(var i=0; i<extractorsFiles.length; i++) {
+    extractors.push(require(path.join(extractorsDir, extractorsFiles[i])));
+  }
+
+  result = [];
+
+  try {
+    uri = new URI(url).protocol('http').normalize();
+  } catch (e) {
+    callback(e, result);
+    return;
+  }
+
+  iterator = function(extractor, done) {
+    extractor.parse(url, uri, function(err, items) {
+      result = result.concat(items);
+      if (err) err += ' ' + url;
+      done(err);
+    });
+  };
+
+  async.eachSeries(extractors, iterator, function(err) {
+    callback(err, result);
+  });
+};
